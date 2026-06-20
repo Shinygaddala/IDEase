ea0758f98faf956885ca37faeac9bc27375b28bd
Shinygaddala <shinygaddala84@gmail.com>
2026-06-20 16:49:18 +0530

    Fixed depenency issues⚙️

--- DigitalIdSystem/src/main/java/com/dis/DigitalIdSystemApplication.java
+++ DigitalIdSystem/src/main/java/com/dis/DigitalIdSystemApplication.java
@@ -1,6 +1,7 @@
 package com.dis;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.github.cdimascio.dotenv.Dotenv;
